8c8adfb0c37cb495d902ccdf280d95bd5a81807b
yngvemag <yngvem@gmail.com>
2024-12-09 18:20:05 +0100

    added comment

--- src/transforms/custom_transforms.py
+++ src/transforms/custom_transforms.py
@@ -5,6 +5,13 @@ from dataclasses import dataclass, field
 from typing import Tuple, Callable
 import constants
 
+import torch
+from torchvision import transforms
+from PIL import Image
+from dataclasses import dataclass, field
+from typing import Tuple, Callable
+import constants
+
 @dataclass
 class CustomTransform:
     image_size: Tuple[int, int] = constants.IMAGE_SIZE
@@ -17,13 +24,20 @@ class CustomTransform:
     def __post_init__(self):
         self.transform = transforms.Compose([
             transforms.Resize(self.image_size),
-            transforms.RandomHorizontalFlip(),
+            transforms.RandomHorizontalFlip(), # Random horizontal flip increases variety
+            # adjust brightness, contrast, saturation, hue
             transforms.ColorJitter(brightness=self.brightness, contrast=self.contrast,
                                    saturation=self.saturation, hue=self.hue),
             transforms.ToTensor(),
+
+            # ImageNet standardization
+            # Normalization helps get data within a range and reduces the skewness which helps learn faster and better
+            # https://pytorch.org/vision/stable/models.html
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
     def __call__(self, img: Image.Image) -> torch.Tensor:
         return self.transform(img)
+
+
 
